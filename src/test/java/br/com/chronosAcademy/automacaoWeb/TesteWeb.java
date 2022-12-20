8cec00791a40b133624e89154f7867ea5644800b
Emerson Gomes da Silva <emgomes@foursys.com.br>
2022-12-20 11:26:39 -0300

    itens da aula 07 de xpath e cssSelector

--- src/test/java/br/com/chronosAcademy/automacaoWeb/TesteWeb.java
+++ src/test/java/br/com/chronosAcademy/automacaoWeb/TesteWeb.java
@@ -25,7 +25,8 @@ public class TesteWeb {
     }
     @Test
     public void primeiroTeste(){
-        String xpathTitulo = "/html/body/div/div/div/div/div/section[2]/div[3]/div/div/div/div/div[1]/div/h4";
+        String xpathTitulo = "//section[2]//h4";
+         //section[2]/div[3]/div/div/div/div/div[1]/div/h4
         WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo));
         String titulo = txtTitulo.getText();
         assertEquals("Porque Tempo É Conhecimento",titulo);
