679767fdc3e3c3c54e4d1989a9cb4c7ee4b1d470
Emerson Gomes da Silva <emgomes@foursys.com.br>
2022-12-23 11:47:43 -0300

    aula 10 page factory

--- src/test/java/br/com/chronosAcademy/automacaoWeb/TesteWeb.java
+++ src/test/java/br/com/chronosAcademy/automacaoWeb/TesteWeb.java
@@ -11,7 +11,6 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import java.time.chrono.ChronoLocalDate;
 
 public class TesteWeb {
 
