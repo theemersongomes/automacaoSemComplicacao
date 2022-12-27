72efc9f1f06756165976fd5d5c17f4f4f7515070
Emerson Gomes da Silva <emgomes@foursys.com.br>
2022-12-27 14:56:24 -0300

    aula 14 runner steps

---
+++ src/test/java/br/com/chronosAcademy/steps/LoginSteps.java
@@ -0,0 +1,77 @@
+package br.com.chronosAcademy.steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class LoginSteps {
+    @Dado("que a modal esteja sendo exibida")
+    public void queAModalEstejaSendoExibida() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Quando("for realizado um clique fora da modal")
+    public void forRealizadoUmCliqueForaDaModal() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Entao("a janela modal deve ser fechada")
+    public void aJanelaModalDeveSerFechada() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Quando("for realizado um clique no icone de fechar")
+    public void forRealizadoUmCliqueNoIconeDeFechar() {
+        
+    }
+
+    @Quando("for realizado um clique no link Create New Account")
+    public void forRealizadoUmCliqueNoLinkCreateNewAccount() {
+        
+    }
+
+    @Entao("a janela moral deve ser fechada")
+    public void aJanelaMoralDeveSerFechada() {
+        
+    }
+
+    @Entao("a janela moral deve ser fechada")
+    public void aJanelaMoralDeveSerFechada() {
+        
+    }
+
+    @Entao("a pagina Create new Account deve ser exibida")
+    public void aPaginaCreateNewAccountDeveSerExibida() {
+        
+    }
+
+    @Quando("os campos de login forem preenchidos da seguinte forma")
+    public void osCamposDeLoginForemPreenchidosDaSeguinteForma() {
+        
+    }
+
+    @Quando("for realizado um clique no botão sign in")
+    public void forRealizadoUmCliqueNoBotãoSignIn() {
+        
+    }
+
+    @Entao("deve ser possivel logar no sistema")
+    public void deveSerPossivelLogarNoSistema() {
+        
+    }
+
+    @Entao("o sistema deve exibir uma mensagem de erro")
+    public void oSistemaDeveExibirUmaMensagemDeErro() {
+        
+    }
+
+    @Quando("os campos de login sejam preenchidos da seguinte forma")
+    public void osCamposDeLoginSejamPreenchidosDaSeguinteForma() {
+        
+    }
+
+    @Entao("o botao sign in deve permanecer desabilitado")
+    public void oBotaoSignInDevePermanecerDesabilitado() {
+    }
+}
