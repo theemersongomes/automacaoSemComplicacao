8840c5da4d1147113ac68329c44ac9a5d600eeaf
Emerson Gomes da Silva <emgomes@foursys.com.br>
2022-12-28 19:23:04 -0300

    aula 15 prefixo pages e maps

---
+++ src/test/java/br/com/chronosAcademy/maps/LoginMap.java
@@ -0,0 +1,17 @@
+package br.com.chronosAcademy.maps;
+
+import com.github.dockerjava.core.exec.WaitContainerCmdExec;
+import org.openqa.selenium.WebElement;
+
+public class LoginMap {
+
+    public WebElement btnLogin;
+    public WebElement btnFechar;
+    public WebElement divFecharModal;
+    public WebElement inpUserName;
+    public WebElement inpPassword;
+    public WebElement btnSignIn;
+    public WebElement inpRemember;
+    public WebElement linkCreateAccount;
+
+}
